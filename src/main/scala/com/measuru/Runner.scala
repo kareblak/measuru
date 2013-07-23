8e5c0a7f060d2555a0ea4ac46487c1274904876e
karblaks <kare.blakstad@finn.no>
2013-07-23 11:14:12 +0200

    dynamic port choice

--- src/main/scala/com/measuru/Runner.scala
+++ src/main/scala/com/measuru/Runner.scala
@@ -1,7 +1,11 @@
 package com.measuru
 
+import scala.util.Properties
+
 object Runner extends App {
 
-  unfiltered.jetty.Http(8889).plan(new Routes).run(_ => println("server started"))
+  val port = Properties.envOrElse("PORT", "8889").toInt
+
+  unfiltered.jetty.Http(port).plan(new Routes).run(_ => println("server started"))
 
 }
